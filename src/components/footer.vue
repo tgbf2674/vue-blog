a3cb8328827c9282e498bffc15fda1020fc109b8
tgbf2674 <tgbf2020@163.com>
2020-12-09 18:04:11 +0800

    commit

---
+++ src/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+    <p>© https://github.com/tgbf2674 wechat: 13726216932</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+</style>
